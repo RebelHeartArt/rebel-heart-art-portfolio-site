d4d43e1dfde03c8bc0904ccf3133a236b8d5070f
Rebel Heart Art <rebelheart1170@gmail.com>
2025-07-07 16:22:08 -0700

    Initial commit

---
+++ javascript/scripts.js
@@ -0,0 +1,10 @@
+console.log("Welcome to Sandi's Portfolio!")
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
