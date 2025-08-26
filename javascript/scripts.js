d4369e810eda52965dda484524d000d35154e1f0
Rebel Heart Art <rebelheart1170@gmail.com>
2025-08-26 15:42:57 -0700

    Revert "Add JavaScript for Nav"
    This reverts commit c70e8a35688b8ff5a5d052ac66a8373ac2811082.

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -8,13 +8,3 @@ function menuToggle() {
         x.className = 'navtoggle';
     }
 }
-
-function handleNavState() {
-  var x = document.getElementById('myNavtoggle');
-  if (window.innerWidth < 640) { // Or whatever your mobile breakpoint is
-    x.className = 'navtoggle'; // Reset to default state
-  }
-}
-
-window.onload = handleNavState;
-window.onresize = handleNavState;
